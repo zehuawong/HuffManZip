333470c6480b51cdd044e18c2d067e724ce5cd11
zehua <2561871246@qq.com>
2018-03-22 14:40:36 +0800

    Create 单词压缩优化.cpp

---
+++ 单词压缩优化.cpp
@@ -0,0 +1,458 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<map>
+#include<queue>
+#include<time.h>
+#include<iomanip>
+using namespace std;
+
+#define TEST_NUM 20 //测试次数
+
+struct  HuffNode
+{
+	string value;//单词
+	int freq; //单词出现频率
+	string huffcode;//编码串
+	HuffNode* left;
+	HuffNode* right;
+};
+
+struct cmpByFreq {
+
+	cmpByFreq() {}
+	bool operator()(const HuffNode *node1, const HuffNode *node2)const {
+		return node1->freq > node2->freq;//根据字符频率最小值优先
+	}
+};
+//生成Huffman 01编码串的函数
+void makeHuffmanCode(HuffNode *node, string codestr)
+{
+	node->huffcode += codestr;
+	if (node->left != NULL)
+		makeHuffmanCode(node->left, node->huffcode + "0");
+	if (node->right != NULL)
+		makeHuffmanCode(node->right, node->huffcode + "1");
+
+}
+//输出Huffman树叶节点的函数
+void printLeaf(HuffNode * node)
+{
+	if (node) {
+		printLeaf(node->left);
+		printLeaf(node->right);
+		if (node->left == NULL || node->right == NULL)
+			cout << node->value << " : " << node->freq << " " << node->huffcode << endl;
+	}
+}
+void printCharToBin(char tempchar)
+{
+	int T = 8;
+	while (T--) //输出二进制串
+	{
+		if ((tempchar&(1 << T)))
+			cout << 1;
+		else cout << 0;
+	}
+	cout << endl;
+}
+//释放内存
+void destroy(HuffNode *node)
+{
+	if (node) {
+		destroy(node->left);
+		destroy(node->right);
+		delete node;
+	}
+}
+//压缩文件的函数
+void huffmanZipByWord(string filename,string zipname)
+{
+	ifstream fin;
+	fin.open(filename, ios::binary);
+	if (!fin) {
+		cout << "file open error!" << endl;
+		exit(1);
+	}
+	cout << "正在压缩文件……" << endl;
+	ofstream fout(zipname + ".wzip", ios::binary);
+	string bufferOutStr = "";
+	long fbeg = fin.tellg();
+	fin.seekg(0, ios::end);
+	long fend = fin.tellg();
+	long filelength = fend - fbeg;
+
+	fin.seekg(0, ios::beg);
+	//cout << fbeg << " " << fend << " " << filelength << endl;
+
+	map<string, int> wordMap;
+	char ch;
+	string wordstr = "";
+	string chstr = "";
+	int wordsum = 0;	//单词总数（非种类数）
+	string bufferInStr; //改进方法，一次性读取磁盘文件到内存中的buffstr
+	bufferInStr.assign(istreambuf_iterator<char>(fin), istreambuf_iterator<char>());
+	do {
+		for (int i = 0; i < bufferInStr.size(); i++)
+		{
+			ch = bufferInStr[i];
+			if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z'))
+				wordstr += ch;
+			else {
+				//cout << "字符：" << ch << endl;
+				if (wordstr != "") {
+					if (wordMap.find(wordstr) == wordMap.end())
+						wordMap[wordstr] = 1;
+					else wordMap[wordstr]++;
+					wordstr = "";
+					wordsum++;
+				}
+				chstr = "";   //非字母类型入map
+				chstr += ch;
+				if (wordMap.find(chstr) == wordMap.end())
+					wordMap[chstr] = 1;
+				else wordMap[chstr]++;
+				wordsum++;
+			}
+		}
+	} while (getline(fin, bufferInStr));		
+	fin.close();
+	//最后一个字符的处理
+	if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z')) //如果最后一个字符是字母
+	{
+		if (wordMap.find(wordstr) == wordMap.end())
+			wordMap[wordstr] = 1;
+		else wordMap[wordstr]++;
+		wordstr = "";
+		wordsum++;
+	}
+
+
+	fout.write((char*)&wordsum, sizeof(int)); //向压缩文件写入单词总数
+	int wordNum = wordMap.size(); //获取单词种类
+	fout.write((char*)&wordNum, sizeof(int));//向压缩文件写入单词种类数
+	//cout << endl<<wordNum << "种单词" << endl;
+
+	char spacech = ','; //分隔符
+	//获取最小优先队列
+	map<string, HuffNode*> huffmanWordMap;
+	map<string, int>::iterator iter;
+	priority_queue<HuffNode*, vector<HuffNode*>, cmpByFreq> min_queue;
+	for (iter = wordMap.begin(); iter != wordMap.end(); iter++)
+	{
+		HuffNode *huffNode = new HuffNode();
+		huffNode->left = NULL; huffNode->right = NULL;
+		huffNode->value = iter->first;
+		huffNode->freq = iter->second;
+		min_queue.push(huffNode);
+		huffmanWordMap[huffNode->value] = huffNode;
+		//cout << huffNode->value << " : " << huffNode->freq << endl;
+		fout.write((char*)&huffNode->freq, sizeof(int)); //写入单词-频率对
+		//fout.write((char*)&huffNode->value,huffNode->value.size());		//注意不能用sizeof(string)
+		//fout.write(string)这种方式当字符串很长的时候也会发生乱码错误
+		fout << huffNode->value;
+		fout.write((char*)&spacech, sizeof(char));//写一个spacech作为分隔符
+
+	}
+
+	//构造哈夫曼树
+	HuffNode *node1, *node2, *root = NULL;
+	for (int i = 0; i < wordNum - 1; i++) {	//n-1次
+		HuffNode *newNode = new HuffNode();
+		if (min_queue.size() == 2)	//为了避免每次循环都要判断，可以先循环处理n-2次，最后一次再单独处理
+			root = newNode;
+		node1 = min_queue.top(); min_queue.pop();
+		node2 = min_queue.top(); min_queue.pop();
+		newNode->left = node1;
+		newNode->right = node2;
+		newNode->freq = node1->freq + node2->freq;//权重之和
+												  //cout << node1->freq << " " << node2->freq <<" "<<newNode->freq << endl;
+		min_queue.push(newNode);
+	}
+	 
+	makeHuffmanCode(root, ""); //生成各个字符的Huffman编码串
+	//printLeaf(root);
+
+	//写入Huffman编码到压缩文件
+	wordstr = "";
+	int length = 8; char tempchar = 0;
+	ifstream infile(filename, ios::binary);
+	string codestr="";
+	bufferOutStr = "";
+	
+	bufferInStr.assign(istreambuf_iterator<char>(infile), istreambuf_iterator<char>());
+	do {
+		for (int j = 0; j < bufferInStr.size(); j++)
+		{
+			ch = bufferInStr[j];
+			if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z'))
+				wordstr += ch;
+			else {
+				//cout << "字符：" << ch << endl;
+				if (wordstr != "") {		//单词类型
+					codestr = huffmanWordMap[wordstr]->huffcode;
+					//cout << wordstr<<" : "<<codestr << endl;
+					for (int i = 0; i < codestr.size(); i++) {
+						length--;
+						if (codestr[i] == '0')
+							tempchar = tempchar | (0 << length);
+						else tempchar = tempchar | (1 << length);
+						if (length == 0) {
+							//fout.write(&tempchar, sizeof(char));
+							bufferOutStr += tempchar;
+							//写入Huffman编码串到压缩文件	
+							//printCharToBin(tempchar); 
+							length = 8;
+							tempchar = 0;
+						}
+					}
+					wordstr = "";
+				}
+				//非字母类型
+				chstr = ""; chstr += ch;
+				codestr = huffmanWordMap[chstr]->huffcode;
+				for (int i = 0; i < codestr.size(); i++) {
+					length--;
+					if (codestr[i] == '0')
+						tempchar = tempchar | (0 << length);
+					else tempchar = tempchar | (1 << length);
+					if (length == 0) {
+						//fout.write(&tempchar, sizeof(char));
+						bufferOutStr += tempchar;
+						//写入Huffman编码串到压缩文件	
+						//printCharToBin(tempchar);
+						length = 8;
+						tempchar = 0;
+					}
+				}
+			}
+		}
+
+	} while (getline(infile, bufferInStr)); 
+	//如果最后一个字符是字母
+	if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z')) {
+		codestr = huffmanWordMap[wordstr]->huffcode;
+		for (int i = 0; i < codestr.size(); i++) {
+			length--;
+			if (codestr[i] == '0')
+				tempchar = tempchar | (0 << length);
+			else tempchar = tempchar | (1 << length);
+			if (length == 0) {
+				//fout.write(&tempchar, sizeof(char));
+				//写入Huffman编码串到压缩文件
+				bufferOutStr += tempchar;
+				//printCharToBin(tempchar);
+				length = 8;
+				tempchar = 0;
+			}
+		}
+	}
+	if (length != 0) //最后一个字节的处理，防止写入压缩文件的时候长度未写满8位的倍数
+	{		
+		//printCharToBin(tempchar);
+		//fout.write(&tempchar, sizeof(char));
+		bufferOutStr += tempchar;
+	}	
+	
+	fout << bufferOutStr;
+	//释放内存，清除Huffman树每个节点
+	//destroy(root);
+	infile.close();	
+	fout.close();
+	cout << "压缩完成!" << endl;
+}
+
+void unzipHuffFileByWord(string filename)
+{	
+	ifstream fin;
+	fin.open(filename, ios::binary);
+	if (!fin) {
+		cout << "file open error!" << endl;
+		exit(1);
+	}
+	cout << "正在解压……" << endl;
+	ofstream fout(filename + ".unzip", ios::binary);
+	int wordsum = 0;
+	fin.read((char*)&wordsum, sizeof(int)); //获取单词总数
+	int wordNum = 0;
+	fin.read((char*)&wordNum, sizeof(int)); //获取单词种类数
+	string wordstr; int freq; char ch; int spaceflag=0;
+	//cout << wordNum << "种单词" << endl;
+	//最小优先队列 priority_queue
+	priority_queue<HuffNode*, vector<HuffNode*>, cmpByFreq> min_queue;
+	for (int i = 0; i < wordNum; i++)
+	{
+		fin.read((char*)&freq, sizeof(int));
+		wordstr ="";
+		spaceflag = 0;
+		while (true) 
+		{
+			fin.read(&ch, sizeof(char));
+			if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z')) {
+				wordstr += ch;
+			}
+			else {
+				if (wordstr == "") {
+					wordstr += ch;				
+					continue;
+				}										
+				if (wordstr!=""&&ch == ',') //读到分隔符（逗号）//
+					break;
+			}
+		}
+		//cout << wordstr << " : " << freq << endl;
+		HuffNode *huffnode = new HuffNode();
+		huffnode->left = NULL; huffnode->right = NULL;
+		huffnode->value = wordstr;
+		huffnode->freq = freq;
+		//入队列
+		min_queue.push(huffnode);
+	}
+	//构造哈夫曼树
+	HuffNode *node1, *node2, *root = NULL;
+	for (int i = 0; i < wordNum - 1; i++) {	//n-1次
+		HuffNode *newNode = new HuffNode();
+		if (min_queue.size() == 2)	//为了避免每次循环都要判断，可以先循环处理n-2次，最后一次再单独处理
+			root = newNode; //更新根节点指针
+		node1 = min_queue.top(); min_queue.pop();
+		node2 = min_queue.top(); min_queue.pop();
+		newNode->left = node1;
+		newNode->right = node2;
+		newNode->freq = node1->freq + node2->freq;//权重之和
+											  //cout << node1->freq << " " << node2->freq <<" "<<newNode->freq << endl;
+		min_queue.push(newNode);
+	}
+	if (root == NULL)
+		return;
+	makeHuffmanCode(root, ""); //生成各个单词的Huffman编码串
+	//printLeaf(root);
+	//开始解码
+	char tempchar = 0; 
+	HuffNode *tempNode = root; //初始指向根节点 //注意要防止最后一个字节编码读两次（用wordsum--控制）
+	
+	string bufferInStr = "";
+	string bufferOutStr = "";
+	bufferInStr.assign(istreambuf_iterator<char>(fin), istreambuf_iterator<char>());
+	do {
+		for (int j = 0; j < bufferInStr.size(); j++)
+		{
+			ch = bufferInStr[j];
+			for (int i = sizeof(char) * 8 - 1; i >= 0; i--)
+			{
+				if (tempNode->left != NULL)
+				{
+					if ((ch >> i) & 1)
+						tempNode = tempNode->right;
+					else tempNode = tempNode->left;
+				}
+				if (tempNode->left == NULL) //找到叶节点则将单词写入文件
+				{
+					if (wordsum == 0)
+						break;
+					wordsum--;
+					wordstr = tempNode->value;
+					//fout.write((char*)&wordstr, sizeof(wordstr));//注意： 用sizeof(string)是有错误的
+					//无论你的string里放多长的字符串，它的sizeof()都是固定的
+					//fout.write((char*)&wordstr, wordstr.size()); //这种方法也是错误的
+					//fout << wordstr;
+					bufferOutStr += wordstr;
+					//cout <<wordstr;
+					tempNode = root; //将tempNode重新指向根节点
+				}
+			}
+		}
+	} while (getline(fin, bufferInStr));
+	 
+	fout << bufferOutStr;
+	
+	cout << "解压完成！" << endl;
+	//释放内存，释放Huffman树的每一个节点内存
+	//destroy(root);
+	fin.close();
+	fout.close();
+}
+
+void compareFiles(string srcfile, string zipfile)
+{
+
+	ifstream srcfin(srcfile);
+	ifstream zipfin(zipfile);
+	long fbeg1 = srcfin.tellg();
+	srcfin.seekg(0, ios::end);
+	long fend1 = srcfin.tellg();
+
+	long fbeg2 = zipfin.tellg();
+	zipfin.seekg(0, ios::end);
+	long fend2 = zipfin.tellg();
+
+	cout << srcfile << "大小：" << (fend1 - fbeg1)*1.0 / 1024 << "kB" << endl;
+	cout << zipfile << "大小：" << (fend2 - fbeg2)*1.0 / 1024 << "kB" << endl;
+	cout << setiosflags(ios::fixed) << setprecision(2);
+	cout << "压缩率：" << (fend2 - fbeg2) *1.0 / (fend1 - fbeg1) * 100 << "%" << endl;
+
+	srcfin.close();
+	zipfin.close();
+}
+
+void testWriteStrToFile(string filename) //测试向文件写入长字符串是否有乱码错误
+{
+	ofstream fout(filename, ios::binary);
+	string str="abcabctrationfor";
+	//string str = "hello";
+	cout << "size:" << str.size() << endl;
+	//fout.write((char*)&str,str.size()); //如果字符串长度过长，这种方式写入文件则会发生错误
+	fout << str; //这种方式是正确的
+	fout.close();
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc!=3)
+	{
+		cout << "命令格式错误！" << endl;
+		cout << "压缩命令格式应该为：huffmanZipByWord filename zipfilename" << endl;
+		//cout << "解压缩命令格式应该为：unzipHuffFileByWord filename" << endl;
+		exit(1);
+	}
+	clock_t startTime, endTime, sumTime = 0;
+	startTime = clock();
+	string filename = argv[1];
+	string zipfilename = argv[2];
+	huffmanZipByWord(argv[1], argv[2]);
+	//unzipHuffFileByWord(argv[1]);
+	endTime = clock();
+	cout << "用时：" << (endTime - startTime)*1.0 / 1000.0 << "s" << endl;
+
+	
+	
+/*
+	clock_t startTime, endTime,sumTime=0;
+	//string filename = "files/oldfile.txt";
+	//string filename = "files/Martin Luther King - I Have A Dream.txt";
+	//string filename = "files/老人与海英文版.txt";
+	//string filename = "files/老人与海英文版.txt";
+	//string filename = "files/生命不能承受之轻（英文版）.txt";
+	//string filename = "files/Pride and Prejudice.txt";
+	//string filename = "files/百年孤独-英文版.txt";
+	//string filename = "files/傲慢+百年+麦田.txt";
+	//string filename = "files/万历十五年.txt";
+	string filename = "files/明朝那些事儿.txt";
+	//string filename = "files/cacm.all";
+				
+	for (int i = 0; i < TEST_NUM; i++)
+	{
+		startTime = clock();
+		huffmanZipByWord(filename,filename);
+		endTime = clock();
+		sumTime += (endTime - startTime);
+		cout << (endTime - startTime) << "ms ";
+	}
+	cout << endl;
+	unzipHuffFileByWord(filename + ".wzip");
+	compareFiles(filename, filename + ".wzip");
+
+	cout << "用时"<<sumTime*1.0 /TEST_NUM/1000.0 << "s" << endl;
+	system("pause");
+	*/
+	return 0;
+}
